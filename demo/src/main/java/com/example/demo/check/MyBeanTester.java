c4c183697baddb4637232a7432249e1b4bd2ce08
MyNameIsDeepak25 <deepakkhedkar31@gmail.com>
2026-01-20 18:55:30 +0530

    Day 1 Practice

---
+++ demo/src/main/java/com/example/demo/check/MyBeanTester.java
@@ -0,0 +1,20 @@
+package com.example.demo.check;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+@Component
+public class MyBeanTester {
+
+	@PostConstruct
+	 public void checkPostConstruct() {
+		System.out.println("Checking custom init method");
+	}
+	
+	@PreDestroy
+	public void checkPredestroy() {
+		System.out.println("Bean is destroyed");
+	}
+}
